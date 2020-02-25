c77d1c0ac806c490cc7970a1dc3eb35c054d065a
Richard Liao <liao098@gmail.com>
2020-02-25 22:59:00 +0000

    implemented js to create tweet articles

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,37 +3,103 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(() => {
+  // https://stackoverflow.com/a/3177838/6024104
+  const timeSince = date => {
+    const seconds = Math.floor((new Date() - date) / 1000);
+    let interval = Math.floor(seconds / 31536000);
 
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac"
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants"
-  },
-  created_at: 1461116232227
-};
+    if (interval > 1) {
+      return interval + " years";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + " months";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + " days";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + " hours";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
+  };
 
-const createTweetElement = () => {};
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac"
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants"
+      },
+      created_at: 1461116232227
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd"
+      },
+      content: {
+        text: "Je pense , donc je suis"
+      },
+      created_at: 1461113959088
+    }
+  ];
 
-const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").append($tweet);
+    }
+  };
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  const createTweetElement = tweetData => {
+    const tweetHTML = `
+    <article class="tweet">
+      <header class="header">
+        <div class="avatar-sm justify-end">
+          <img src="${tweetData.user.avatars}" />
+        </div>
+        <div class="avatar-name">
+          <h4>${tweetData.user.name}</h4>
+        </div>
+        <div class="avatar-handle justify-end">
+          <p>${tweetData.user.handle}</p>
+        </div>
+      </header>
+      <div class="body">
+        <p class="tweet-text">
+          ${tweetData.content.text}
+        </p>
+      </div>
+      <footer class="footer">
+        <div class="tweet-timeposted">
+          <p>${timeSince(tweetData.created_at)} ago</p>
+        </div>
+        <div class="tweet-social justify-end">
+          <a class="tweet-share" href="share">share</a>
+          <a class="tweet-repost" href="repost">repost</a>
+          <a class="tweet-like" href="like">like</a>
+        </div>
+      </footer>
+    </article>
+    `;
 
-// const renderTweets = function(tweets) {
-//   // loops through tweets
-//   // calls createTweetElement for each tweet
-//   // takes return value and appends it to the tweets container
-// }
+    return tweetHTML;
+  };
 
-// const createTweetElement = function(tweet) {
-//   let $tweet = $('<article>').addClass('tweet');
-//   // ...
-//   return $tweet;
-// }
-
-// renderTweets(data);
+  renderTweets(data);
+});
